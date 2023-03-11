f3b2a38e52a7457ebe64d9fb138e9119c1aa99b6
Ivan42210 <ivan.cha.42@outlook.fr>
2023-03-11 19:29:35 +0100

    cleaning

--- scripts/utils/search.js
+++
@@ -1,9 +0,0 @@
-export function advancedFilters(arrays, value, output) {
-
-
-    const result = arrays.filter(function(arr) { return arr.name.toLowerCase().indexOf(value.toLowerCase()) != -1 })
-
-    output.push(result)
-
-
-}
